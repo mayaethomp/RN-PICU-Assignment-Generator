170dd4d492ae32b349cf532450826556c42417a8
CodeDoses <mayaethomp@gmail.com>
2026-01-03 20:25:28 -0800

    added javadoc comments for restriction enums for clarity

--- app/src/main/java/rn/picu/assignment/generator/domain/Restriction.java
+++ app/src/main/java/rn/picu/assignment/generator/domain/Restriction.java
@@ -1,9 +1,33 @@
 package rn.picu.assignment.generator.domain;
 
+/**
+ * Represents assignment restrictions that limit which patient populations
+ * a nurse may be assigned to.
+ */
 public enum Restriction {
+
+  /**
+   * Nurse may not be assigned to patients requiring CVVH.
+   */
   NO_CVVH,
+
+  /**
+   * Nurse may not be assigned to patients receiving chemotherapy.
+   */
   NO_CHEMO,
-  LESS_THAN_30kg,
+
+  /**
+   * Nurse may not be assigned to patients weighing less than 30 kilograms.
+   */
+  LESS_THAN_30KG,
+
+  /**
+   * Nurse may not be assigned to patients requiring certain isolation precautions.
+   */
   ISOLATION,
+
+  /**
+   * Nurse may not be assigned to patients receiving radiation therapy.
+   */
   RADIATION
 }
