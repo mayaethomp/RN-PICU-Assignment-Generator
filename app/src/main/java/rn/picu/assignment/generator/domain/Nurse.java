2344875cd984c516eb1f7395140ce98aff65527a
Maya Thompson <mayaethomp@gmail.com>
2026-01-03 17:50:47 -0800

    added UUID to nurse class to assign each nurse object its own unique id

--- app/src/main/java/rn/picu/assignment/generator/domain/Nurse.java
+++ app/src/main/java/rn/picu/assignment/generator/domain/Nurse.java
@@ -1,32 +1,66 @@
 package rn.picu.assignment.generator.domain;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
+
+/**
+ * 
+ */
 
 public class Nurse {
-  private final int id;
+  private final UUID id;
   private final String name;
   private final LocalDate unitStartDate; 
   private final UnitType homeUnit;
 
   private final Set<Skill> skills;
   private final Set<Restriction> restrictions;
-
   
 
   public Nurse (String name,
-                int id, 
+                UUID id, 
                 LocalDate unitStartDate, 
                 UnitType homeUnit,
                 Set<Skill> skills,
                 Set<Restriction> restrictions
   ) {
-    this.name = name;
-    this.id = id;
-    this.unitStartDate = unitStartDate; 
+    this.name = Objects.requireNonNull(name);
+    this.id = Objects.requireNonNull(id);
+    this.unitStartDate = Objects.requireNonNull(unitStartDate); 
     this.homeUnit = homeUnit;
     this.skills = Set.copyOf(skills);
     this.restrictions = Set.copyOf(restrictions);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public UnitType getHomeUnit() {
+    return homeUnit;
+  }
+
+  public LocalDate getStartDate() {
+    return unitStartDate;
+  }
+
+  public Set<Skill> getSkills() {
+    return skills;
+  }
+
+  public Set<Restriction> getRestrictions() {
+    return restrictions;
+  }
+
+  @Override
+  public String toString() {
+    return "RN: " + name + ", " + homeUnit + " | Employee ID: " + id + " |  Skills: " + skills + " | Restrictions: " + restrictions;
   }
 
 
