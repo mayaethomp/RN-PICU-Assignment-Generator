7177884c680d83e45f06eee959539fe5c3c1224f
CodeDoses <mayaethomp@gmail.com>
2026-01-05 00:31:45 -0800

    Added javadoc comments to UnitType

--- app/src/main/java/rn/picu/assignment/generator/domain/UnitType.java
+++ app/src/main/java/rn/picu/assignment/generator/domain/UnitType.java
@@ -1,8 +1,26 @@
 package rn.picu.assignment.generator.domain;
 
+/**
+ * Represents units of the hospitals that nurses are hired to and work in on a regular basis.
+ */
 public enum UnitType {
+  /**
+   * Pediatric Intesive Care Unit
+   */
   PICU,
+
+  /**
+   * Newborn and Infant Intensive Critical Care Unit
+   */
   NICCU,
+
+  /**
+   * Cardiothoracic Intesive Care Unit
+   */
   CTICU,
+
+  /**
+   * Float Team
+   */
   FLOAT
 }
