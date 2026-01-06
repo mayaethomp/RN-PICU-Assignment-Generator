e4db3da3c256ae45411a39886fa0e583c9d72c61
Maya Thompson <mayaethomp@gmail.com>
2026-01-06 02:58:48 -0800

    added javadocs to skill.java

--- app/src/main/java/rn/picu/assignment/generator/domain/Skill.java
+++ app/src/main/java/rn/picu/assignment/generator/domain/Skill.java
@@ -1,8 +1,31 @@
 package rn.picu.assignment.generator.domain;
 
+/**
+ * Represents skills that can be acquired by the nurse through 
+ * training and/or certification and are required to be assigned to 
+ * some patients.
+ */
 public enum Skill {
+  /**
+   * Continuous veno-venous hemofiltration, also called CVVH, is a 
+   * form of continuous renal (or kidney) replacement therapy that 
+   * is like dialysis (also called CRRT)
+   */
   CVVH,
+  /**
+   * Chemotherapy; requires certification
+   */
   CHEMO, 
+  /**
+   *  The Carpediem™ system; intended for continuous renal 
+   * replacement therapy for pediatric patients weighing 
+   * between 2.5 and 10 kilograms.
+   */
   CARPEDIEM,
+
+  /**
+   * Charge nurse; oversees operations of the nursing unit during a 
+   * shift and assigns nurses to patients for the following shift
+   */
   CHARGE
 }
