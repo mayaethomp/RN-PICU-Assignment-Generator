eac8d68246dd372db1f388fcf5d0fcadebb49a76
CodeDoses <mayaethomp@gmail.com>
2026-01-05 22:03:34 -0800

    Added three classes to represent where a patient is located: RoomNumber, BedLocation, and BedSide

---
+++ app/src/main/java/rn/picu/assignment/generator/domain/BedLocation.java
@@ -0,0 +1,5 @@
+package rn.picu.assignment.generator.domain;
+
+public record BedLocation(RoomNumber room, BedSide bedSide) {
+  
+}
---
+++ app/src/main/java/rn/picu/assignment/generator/domain/BedSide.java
@@ -0,0 +1,5 @@
+package rn.picu.assignment.generator.domain;
+
+public enum BedSide {
+  A, B
+}
