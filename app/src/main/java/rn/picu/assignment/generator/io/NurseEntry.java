bc1a5ea9675093c5a293c7a2fa6106ca45ec13e9
Maya Thompson <mayaethomp@gmail.com>
2026-01-13 21:19:59 -0800

    Adding an entry class for nurses to be able to store parsed Nurse data

---
+++ app/src/main/java/rn/picu/assignment/generator/io/NurseEntry.java
@@ -0,0 +1,15 @@
+package rn.picu.assignment.generator.io;
+
+import java.util.List;
+
+import rn.picu.assignment.generator.domain.classifications.Restriction;
+import rn.picu.assignment.generator.domain.classifications.Skill;
+import rn.picu.assignment.generator.domain.classifications.UnitType;
+import rn.picu.assignment.generator.domain.entities.Nurse;
+
+public class NurseEntry {
+  private Nurse nurse;
+  private List<Skill> skills;
+  private List<Restriction> restrictions;
+  private UnitType homeUnit;
+}
